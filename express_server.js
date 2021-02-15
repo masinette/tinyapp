c72adcd89e7bf0c8593979d56862c26ade459357
masinette <samuelstoniann@gmail.com>
2021-02-15 15:08:17 -0500

    setup server

--- express_server.js
+++ express_server.js
@@ -11,6 +11,12 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+//have the server listen for incoming requests
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
