2a660c15274e88a3e116430b30016b204d03b45e
masinette <samuelstoniann@gmail.com>
2021-02-15 17:13:07 -0500

    last commit of exercise

--- express_server.js
+++ express_server.js
@@ -15,6 +15,22 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
+
+
+
+
 
 //have the server listen for incoming requests
 app.listen(PORT, () => {
